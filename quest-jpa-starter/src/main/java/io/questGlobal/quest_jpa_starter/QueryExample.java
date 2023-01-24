38c850bc3b8819552f0fa224c24fde57a0b0f81f
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2023-01-24 16:25:48 +0530

    typed query

---
+++ quest-jpa-starter/src/main/java/io/questGlobal/quest_jpa_starter/QueryExample.java
@@ -0,0 +1,29 @@
+package io.questGlobal.quest_jpa_starter;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import javax.persistence.TypedQuery;
+
+public class QueryExample {
+	public static void main(String[] args) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("myApp");
+		EntityManager em = emf.createEntityManager();
+
+		//get all records
+		//		TypedQuery<Employee> query = em.createQuery("select e from Employee e",Employee.class);
+		//employee-> class name, not the table name
+
+		// condition: where age >25
+		TypedQuery<Employee> query = em.createQuery("select e from Employee e where age > 25 order by e.age ",Employee.class);
+
+		List<Employee> resultList = query.getResultList();
+		resultList.forEach(System.out::println);
+		emf.close();
+		em.close();
+
+	}
+}
