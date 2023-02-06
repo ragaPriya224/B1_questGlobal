4a2488be50048ff85c558b45a46e60dfbcdc6f00
91763 <91763@GG>
2023-02-06 15:03:37 +0530

    samsung class create, use app context

--- springCodes/AnnoDemo/src/main/java/org/springDemo/AnnoDemo/App.java
+++ springCodes/AnnoDemo/src/main/java/org/springDemo/AnnoDemo/App.java
@@ -1,13 +1,24 @@
 package org.springDemo.AnnoDemo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ){
+    	
         System.out.println( "Hello World!" );
+        //tight coupling , to be avoided
+//        Samsung s = new Samsung();
+//        s.config();
+        
+        ApplicationContext factory = new AnnotationConfigApplicationContext();
+        Samsung s23= factory.getBean(Samsung.class);
     }
 }
+//diff ways to get bean
+
---
+++ springCodes/AnnoDemo/src/main/java/org/springDemo/AnnoDemo/Samsung.java
@@ -0,0 +1,7 @@
+package org.springDemo.AnnoDemo;
+
+public class Samsung {
+	public void config() {
+		System.out.println("samsung is working");
+	}
+}
