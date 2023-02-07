592a2d44d52b25006ebf4253ceaa539b4972292e
91763 <91763@GG>
2023-02-07 11:08:39 +0530

    initial files added

--- springCodes/AnnoDemo/src/main/java/org/springDemo/AnnoDemo/Android.java
+++ springCodes/AnnoDemo/src/main/java/org/springDemo/AnnoDemo/Android.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Android implements MobileProcessor{
 
 	public void process() {
