7f7d5cdbe46e33debe86f9f2d957849e6ca17761
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2023-02-10 15:40:24 +0530

    paging and sorting

--- springCodes/jpa-starter/src/main/java/com/questGlobal/jpastarter/controller/ProductController.java
+++ springCodes/jpa-starter/src/main/java/com/questGlobal/jpastarter/controller/ProductController.java
@@ -1,8 +1,10 @@
 package com.questGlobal.jpastarter.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,25 +22,32 @@ public class ProductController {
 
 	@Autowired
 	private ProductService productService;
-	
-//(localhost:8085/api/v1/products/search?query=card)
+
+	//(localhost:8085/api/v1/products/search?query=card)
 	@GetMapping(value = "/search")
 	public  ResponseEntity<List<Product>> searchProducts(@RequestParam("query") String query) {
-		 return ResponseEntity.ok(productService.searchProducts(query));
+		return ResponseEntity.ok(productService.searchProducts(query));
 
 	}
 
 	@PostMapping
 	public  Product createProduct(@RequestBody Product p) {
-		 return productService.create(p);
+		return productService.create(p);
 
 	}
 	@GetMapping
 	public  List<Product> getProducts() {
-		 return productService.findAll();
+		return productService.findAll();
+
+	}
+	@GetMapping("/page")
+	public  Page<Product>  getProducts(@RequestParam("pageNo") Optional<Integer> pageParam,
+			@RequestParam("sort") Optional<String>  sortBy){
+		System.out.println("-----------------------------------------");
+		return productService.getProductsByPage(pageParam,sortBy);
 
 	}
 
 
-	
+
 }
--- springCodes/jpa-starter/src/main/java/com/questGlobal/jpastarter/service/ProductService.java
+++ springCodes/jpa-starter/src/main/java/com/questGlobal/jpastarter/service/ProductService.java
@@ -1,8 +1,12 @@
 package com.questGlobal.jpastarter.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.questGlobal.jpastarter.model.Product;
@@ -10,22 +14,28 @@ import com.questGlobal.jpastarter.repository.ProductRepository;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	ProductRepository productRepository;
 
 	public List<Product> searchProducts(String query) {
 		return productRepository.searchProducts(query);
-		
+
 	}
-	
+
 	public  Product create(Product p) {
 		return productRepository.save(p);
 	}
-	
+
 	public  List<Product> findAll() {
 		return productRepository.findAll();
 
+	}
+
+	public Page<Product> getProductsByPage(Optional<Integer> pageParam, Optional<String> sortBy) {
+		// TODO Auto-generated method stub
+		return	productRepository.findAll(PageRequest.of(pageParam.orElse(0), 3,
+				Direction.ASC,sortBy.orElse("id")));
 	}
 
 
